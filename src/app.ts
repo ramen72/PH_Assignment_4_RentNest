273643eefaf2206ff2f95061bb7e579584c49161
Ramen Biswas <ramen@devsramen.com>
2026-07-07 22:55:56 +0600

    Create authentication middleware.

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,15 @@ import express, { Application, NextFunction, Request, Response } from "express";
 import { authRoute } from "./modules/auth/auth.route";
 import { notFoundHandler } from "./middlewares/notFoundHandler";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler";
+import { auth } from "./middlewares/auth";
+import { Role } from "../generated/prisma/enums";
 const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/api", (req: Request, res: Response) => {
+app.get("/api/auth", auth(Role.TENANT), (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
